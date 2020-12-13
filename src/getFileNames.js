2b1473013abdff201c1962008357873d89bb60c5
Alejandro Dubon <alejandrodubon88@gmail.com>
2020-12-12 22:26:46 -0600

    respaldo

--- src/getFileNames.js
+++ src/getFileNames.js
@@ -1,15 +1,36 @@
 import { userInfo  } from 'os';
+import { readdirSync } from 'fs';
 import pkg from 'csvtojson';
 const { csv } = pkg;
 
 const username = userInfo().username;
-const csvFile = `C:/Users/${username}/OneDrive/Documentos/index.csv`;
-
+const csvFile = `C:/Users/${username}/OneDrive/Documentos/pdfDirectory/index.csv`;
+const path1 = `C:/Users/${username}/OneDrive/Documentos/`;
+const path2 = `C:/Users/${username}/OneDrive/Documentos/pdfDirectory/src`;
+const path3 = `C:/Users/${username}/OneDrive/Documentos/pdfDirectory/result`
 async function generateFileNames() {
     return await csv().fromFile(csvFile);;
 }
 
-const fileNames = await generateFileNames();
-export { fileNames };
+function getPdfFiles(path) {
+    return readdirSync(path);        
+}
+
+function getPdfSrc() {
+    let response = 'null';
+    const documentsDirectory = getPdfFiles(path1);
+    if (documentsDirectory.includes('pdfDirectory')) {
+        const srcDirectory = getPdfFiles(path2)
+        response = srcDirectory;
+    } else {
+        console.log('No sea ha detectado el pdfDirectory...');
+    }
+    return response;
+}
+
+const pdfNames = getPdfSrc();
+const newfileNames = await generateFileNames();
+
+export { newfileNames, pdfNames, path2, path3 };
 
 // const path = `C:/Users/${username}/Downloads/${filename}`;
--- src/pdfUtils.js
+++ src/pdfUtils.js
@@ -15,7 +15,9 @@ async function generatePDFwith3Pages(pathExtract, pathSave) {
   newDocument.insertPage(2, thirdPage);
   
   writeFileSync(pathSave, await newDocument.save());
-  console.log('documento creado con exito...');
+  console.log('archivo creado')
+  await delete3Pages(pathExtract, 0, 1, 2);
+  console.log('');
 }
 
 async function getNumberOfPages(pathExtract) {
@@ -24,5 +26,20 @@ async function getNumberOfPages(pathExtract) {
   return originalpdf.getPageCount();
 }
 
-export { generatePDFwith3Pages, getNumberOfPages};
+async function delete3Pages(pathExtract, a, b, c) {
+  const originalDoc = readFileSync(pathExtract);
+  let originalpdf = await PDFDocument.load(originalDoc);
+  originalpdf.removePage(a);
+  originalpdf.removePage(b);
+  originalpdf.removePage(c);
+  //console.log(await getNumberOfPages(pathExtract)); 
+  writeFileSync(pathExtract, await originalpdf.save());
+  console.log('paginas eliminadas, paginas restantes: ' + await getNumberOfPages(pathExtract)); 
+}
+
+// for (let index = 0; index < 10; index++) {
+//   await delete3Pages('C:/Users/aleja/OneDrive/Documentos/pdfDirectory/src/El libro negro del programador - Rafael Gomez Blanes.pdf');
+// }
+
+export { generatePDFwith3Pages, getNumberOfPages, delete3Pages};
 //generatePDFwith3Pages('C:/Users/aleja/OneDrive/Documentos/Libros/test.pdf', './src/test.pdf').catch(err => console.log(err));
